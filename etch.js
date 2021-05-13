8a9f0ed4da703aeec44a9fafdc36239cff1d1f28
Ryan Moore <ryanm.inbox@gmail.com>
2021-05-13 18:50:29 -0400

    about to put event listener in makeDivs

--- etch.js
+++ etch.js
@@ -18,10 +18,7 @@ document.getElementById("1").innerHTML = "Hello World";
 const test = document.getElementById("1");
 
 test.addEventListener("mouseover", function( event ) {
-  // highlight the mouseover target
   event.target.style.color = "orange";
-
-  // reset the color after a short delay
   setTimeout(function() {
     event.target.style.color = "";
   }, 500);
