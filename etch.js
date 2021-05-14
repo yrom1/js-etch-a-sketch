22859369cc0d1555afb7cdeb581a702fce26d42d
Ryan Moore <ryanm.inbox@gmail.com>
2021-05-14 01:12:15 -0400

    arbitrary number of squares grid with fading colors

--- etch.js
+++ etch.js
@@ -3,22 +3,24 @@ const container = document.querySelector('#container');
 function makeDivs(num) {
     for (let i=0; i<num*num; i++) {
         const div = document.createElement('div');
-        div.textContent = (i+1).toString();
+        div.textContent = "☐";//(i+1).toString();
         let divID = String(i+1)
         div.setAttribute('id', divID);                              
         container.appendChild(div);
 
         const currentDiv = document.getElementById(String(i+1));
         currentDiv.addEventListener("mouseover", function( event ) {
-          event.target.style.color = "orange";
+          event.target.style.backgroundColor = "orange";
           setTimeout(function() {
-            event.target.style.color = "";
+            event.target.style.backgroundColor = "";
           }, 500);
         }, false);
 
     }
 }
 
-let numberDivs = 9;
-container.setAttribute('style', `display: grid; grid-template-columns: repeat(${numberDivs}, 1fr)`);
-makeDivs(9);
+let numberDivs = 20;
+let gridSizePx = 500;
+let widthSquare = gridSizePx/numberDivs;
+container.setAttribute('style', `display: grid; grid-template-columns: repeat(${numberDivs}, ${widthSquare}px); grid-template-rows: repeat(${numberDivs}, ${widthSquare}px); grid-gap: 0; height: ${gridSizePx}px; width: ${gridSizePx}px;`);
+makeDivs(numberDivs);
