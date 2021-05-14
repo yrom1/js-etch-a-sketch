6d0fc9daf0a46f682f2952622bc6c3f8b7944091
Ryan Moore <ryanm.inbox@gmail.com>
2021-05-14 03:11:56 -0400

    before i break it making the reset button

--- etch.js
+++ etch.js
@@ -1,12 +1,10 @@
-const container = document.querySelector('#container');
-const body = document.querySelector('body');
-
 function makeDivs(num) {
     for (let i=0; i<num*num; i++) {
         const div = document.createElement('div');
-        div.textContent = ".";//(i+1).toString();
+        div.textContent = "";//(i+1).toString();
         let divID = String(i+1)
-        div.setAttribute('id', divID);                              
+        div.setAttribute('id', divID);
+        div.classList.add('etch-div');                              
         container.appendChild(div);
 
         const currentDiv = document.getElementById(String(i+1));
@@ -20,12 +18,36 @@ function makeDivs(num) {
     }
 }
 
-let numberDivs = 10;
+function logSubmit(event) {
+  let val = document.getElementById("reset-input").value;
+  let etchDivs = document.getElementsByClassName('etch-div');
+  for (var i = 0; i < etchDivs.length; i++) {
+    etchDivs[i].style.backgroundColor = 'red';
+  }
+  makeGrid(val);
+  event.preventDefault();
+}
+
+function makeGrid(divs = 15) {
+  let gridSizePx = 500;
+  let gridSizeGap = 5;
+  let widthSquare = gridSizePx/numberDivs;
+  let newGridSizePx = gridSizePx + (gridSizeGap * (numberDivs-1));
+  
+  container.setAttribute('style', `display: grid; grid-template-columns: repeat(${numberDivs}, ${widthSquare}px); grid-template-rows: repeat(${numberDivs}, ${widthSquare}px); grid-gap: 0; height: ${newGridSizePx}px; width: ${newGridSizePx}px;  grid-gap: ${gridSizeGap}px ${gridSizeGap}px; border-style: solid; border-color: darkgray; margin-top: 50px;`);
+  makeDivs(divs)
+}
+
+const container = document.querySelector('#container');
+const form = document.getElementById('form');
+
+let numberDivs = 15;
 let gridSizePx = 500;
 let gridSizeGap = 5;
 let widthSquare = gridSizePx/numberDivs;
-let newGridSizePx = gridSizePx + (gridSizeGap * numberDivs);
+let newGridSizePx = gridSizePx + (gridSizeGap * (numberDivs-1));
 
-body.setAttribute.maxWidth = newGridSizePx + 'px';
-container.setAttribute('style', `display: grid; grid-template-columns: repeat(${numberDivs}, ${widthSquare}px); grid-template-rows: repeat(${numberDivs}, ${widthSquare}px); grid-gap: 0; height: ${newGridSizePx}px; width: ${newGridSizePx}px;  grid-gap: ${gridSizeGap}px ${gridSizeGap}px;`);
+container.setAttribute('style', `display: grid; grid-template-columns: repeat(${numberDivs}, ${widthSquare}px); grid-template-rows: repeat(${numberDivs}, ${widthSquare}px); grid-gap: 0; height: ${newGridSizePx}px; width: ${newGridSizePx}px;  grid-gap: ${gridSizeGap}px ${gridSizeGap}px; border-style: solid; border-color: darkgray; margin-top: 50px;`);
+
 makeDivs(numberDivs);
+form.addEventListener('submit', logSubmit);
